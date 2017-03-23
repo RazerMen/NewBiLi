db24b05b9bad6c9b9a1c01037b8a82b4aced5191
RazerMen <823903729@qq.com>
2017-03-23 16:31:07 +0800

    3、发现的话题中心完成点击事件和布局

--- app/src/main/java/com/wuliwei/newbilibili/activity/HuaTiActivity.java
+++ app/src/main/java/com/wuliwei/newbilibili/activity/HuaTiActivity.java
@@ -1,9 +1,32 @@
 package com.wuliwei.newbilibili.activity;
 
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.alibaba.fastjson.JSON;
 import com.wuliwei.newbilibili.R;
 import com.wuliwei.newbilibili.base.BaseActivity;
+import com.wuliwei.newbilibili.bean.HuaTiBean;
+import com.wuliwei.newbilibili.uitls.AppNet;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import okhttp3.Call;
 
 public class HuaTiActivity extends BaseActivity {
+
+    @BindView(R.id.ib_back)
+    ImageButton ibBack;
+    @BindView(R.id.listView)
+    ListView listView;
+
+    private List<HuaTiBean.ListBean> list;
+    private HuaTiAdapter adapter;
 
     @Override
     protected void initListener() {
@@ -12,11 +35,52 @@ public class HuaTiActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        getDataFromNet();
+    }
 
+    private void getDataFromNet() {
+        OkHttpUtils.get().url(AppNet.HUATI_URL).id(100).build().execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+                Log.e("TAG", "失败" + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+                Log.e("TAG", "成功 ");
+                proceessData(response);
+            }
+        });
+    }
+
+    private void proceessData(String json) {
+        HuaTiBean huaTiBean = JSON.parseObject(json, HuaTiBean.class);
+        Log.e("TAG", "解析成功" + huaTiBean.getList());
+        list = huaTiBean.getList();
+
+        if(list != null && list.size() > 0) {
+            //设置适配器
+            adapter = new HuaTiAdapter(this, list);
+            listView.setAdapter(adapter);
+        }
+
+
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(HuaTiActivity.this, "11111 == " + position, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        
     }
 
     @Override
     public int getLayoutId() {
         return R.layout.activity_hua_ti;
+    }
+
+    @OnClick(R.id.ib_back)
+    public void onClick() {
+        finish();
     }
 }
--- app/src/main/java/com/wuliwei/newbilibili/uitls/AppNet.java
+++ app/src/main/java/com/wuliwei/newbilibili/uitls/AppNet.java
@@ -13,4 +13,7 @@ public class AppNet {
 
     //发现
     public static String FIND_URL = "http://app.bilibili.com/x/v2/search/hot?appkey=1d8b6e7d45233436&build=501000&limit=50&mobi_app=android&platform=android&ts=1490014710000&sign=e5ddf94fa9a0d6876cb85756c37c4adc";
+
+    //发现中话题
+    public static String HUATI_URL = "http://api.bilibili.com/topic/getlist?appkey=1d8b6e7d45233436&build=501000&mobi_app=android&page=1&pageSize=20&platform=android&ts=1490015740000&sign=be68382cdc99c168ef87f2fa423dd280";
 }
